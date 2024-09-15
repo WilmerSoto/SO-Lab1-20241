8aa618420870affa0b0c40b79be3640f79b4ccc3
Wilmer Soto <wilmer.soto@udea.edu.co>
2024-09-15 04:30:49 -0500

    pass test #5. missing two arguments functionality.

--- reverse.c
+++ reverse.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #define MAX_INPUT_LENGTH 100
 
 FILE *fptr;
@@ -72,6 +73,19 @@ void tokenToNode(char *userInput, Node **head){
     }
 }
 
+int hardlinkCheck(const char *file1, const char *file2) {
+    struct stat stat1, stat2;
+
+    if (stat(file1, &stat1) != 0) {
+        return 0;
+    }
+
+    if (stat(file2, &stat2) != 0) {
+        return 0;
+    }
+    return (stat1.st_ino == stat2.st_ino && stat1.st_dev == stat2.st_dev);
+}
+
 int main(int argc, char *argv[]) {
     Node *head = NULL;
     char userInput[MAX_INPUT_LENGTH];
@@ -83,11 +97,13 @@ int main(int argc, char *argv[]) {
             tokenToNode(userInput, &head);
         }
         printListReverse(head);
+        exit(0);
     } else if (argc == 2)
     {
         fptr = fopen(argv[1], "r");
         if (fptr == NULL){
             fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");   
+            exit(1);
         }
         
         while (fgets(userInput, MAX_INPUT_LENGTH, fptr))                
@@ -95,17 +111,18 @@ int main(int argc, char *argv[]) {
             tokenToNode(userInput, &head);
         }
         printListReverse(head);
-
+        exit(0);
     } else if (argc == 3)
     {
-        if (strcmp(argv[1], argv[2]) == 0){
-            fprintf(stderr, "reverse: input and output file must differ\n");   
-        }
+        if (strcmp(argv[1], argv[2]) == 0 || hardlinkCheck(argv[1], argv[2])){
+            fprintf(stderr, "reverse: input and output file must differ\n");
+            exit(1);   
+        } 
 
         fptr = fopen(argv[1], "r");
-
         if (fptr == NULL){
-            fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");   
+            fprintf(stderr, "reverse: cannot open file '/no/such/file.txt'\n");
+            exit(1);   
         }
     } else if (argc >= 4)
     {
